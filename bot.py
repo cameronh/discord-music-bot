6c9d3710f96cb42c2a713aa631d59b44ac7d7ed3
Cam <cam@chudson.io>
2021-10-05 13:13:57 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord import voice_client
+from discord.ext import commands,tasks
+import os
+from dotenv import load_dotenv
+from music import Music
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents().default()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+if __name__ == "__main__":
+  bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description='Discord.py music bot')
+  bot.add_cog(Music(bot))
+  bot.run(DISCORD_TOKEN)
